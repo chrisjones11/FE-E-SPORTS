c8959dd99986fe6eb049a8e1ec43355ba4d6f5c2
Ziziou91 <joshgray91@gmail.com>
2017-10-25 11:57:23 +0100

    implement static slideshow for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,14 @@ class Home extends React.Component {
   render() {
     return (
       <div className='screening'>
-        <div className="row ">
+        <div className="row slideshow">
           <div className="col-12">
-            <h1 >Placeholder for slide show</h1>
+            <img className="homeimg" src="https://i.ytimg.com/vi/MzAvNmUn4q0/maxresdefault.jpg"></img>
+          </div>
+        </div>
+        <div className="row gamedisplayheading">
+          <div className="col-12">
+            <h1 >Live Games</h1>
           </div>
         </div>
         <div className='row gamedisplayheading'>
@@ -43,28 +48,36 @@ class Home extends React.Component {
           <div className="col-sm-12">
             <p>Upcoming Tournments</p>
           </div>
-          <div className='row imgrow1'>
-          <div className="col-sm-4 upcomingimgcont">
-          <img className="upcomingimg1" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865956/dota_upm_n4cvol.jpg'/>
+          <div className="row">
+            <div className='col-12'>
+              <table classname="upcomingtable">
+                <tr classname="upcomingtable">
+                  <td classname="upcomingtable">
+                  <img className="upcomingimg1" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865956/dota_upm_n4cvol.jpg'/>
+                  </td>
+                  <td classname="upcomingtable">
+                  <img className="upcomingimg3" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865956/dota_upm_n4cvol.jpg'/>
+                  </td>
+                  <td classname="upcomingtable">
+                  <img className="upcomingimg2" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865956/dota_upm_n4cvol.jpg'/>
+                  </td>
+                </tr>
+                <tr classname="upcomingtable">
+                  <td classname="upcomingtable">
+                  <img className="upcomingimg1" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865956/dota_upm_n4cvol.jpg'/>
+                  </td>
+                  <td classname="upcomingtable">
+                  <img className="upcomingimg3" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865956/dota_upm_n4cvol.jpg'/>
+                  </td>
+                  <td classname="upcomingtable">
+                  <img className="upcomingimg2" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865956/dota_upm_n4cvol.jpg'/>
+                  </td>
+                </tr>
+              </table>
+            </div>
           </div>
-          <div className="col-sm-4 upcomingimgcont">
-          <img className="upcomingimg1" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865846/heroes_up_jvwnnb.jpg'/>
-          </div>
-          <div className="col-sm-4 upcomingimgcont">
-          <img className="upcomingimg1" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865847/overwatch_up_tqpian.png'/>
-          </div>
-          </div>
-          <div className='row imgrow2'>
-          <div className="col-sm-4 upcomingimgcont">
-          <img className="upcomingimg1" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865845/csgo_up_u2v5fm.jpg'/>
-          </div>
-          <div className="col-sm-4 upcomingimgcont">
-          <img className="upcomingimg1" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865845/csgo_upd_jfucnu.jpg'/>
-          </div>
-          <div className="col-sm-4 upcomingimgcont">
-          <img className="upcomingimg1" src='http://res.cloudinary.com/diohwb5lp/image/upload/v1508865848/dota2_up_hidamj.png'/>
-          </div>
-          </div>
+          
+         
           </div>
         </div>
 
