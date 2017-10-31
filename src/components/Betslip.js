42abf0a89deee32167cf0bd0d5bc786017e7f9ee
Sunny <ssharda92@gmail.com>
2017-10-31 15:27:00 +0000

    have added handleplacedbets for front-end

--- src/components/Betslip.js
+++ src/components/Betslip.js
@@ -1,79 +1,106 @@
-import React from 'react';
-import Cardform from './Cardform'
-import './Betslip.css'
-import {connect} from 'react-redux';
-import {insertStake} from '../actions/betslip'
-
+import React from "react";
+import Cardform from "./Cardform";
+import "./Betslip.css";
+import { connect } from "react-redux";
+import { insertStake } from "../actions/betslip";
+import { placeBets } from "../actions/postBets";
+import postBets from "../actions/postBets";
 
 class Betslip extends React.Component {
-    constructor(props){
-        super(props);
-        this.changeReturn = this.changeReturn.bind(this);
-    }
-    changeReturn(val, id){
-     this.props.insertStake(val, id);
-        }
-
-    render () {
-      return (
-         <div className="betslip-outer col-3">
-            <div className='betslip row'>
-                <div className="col-12 betmain">
-                    <div className="row">
-                        <div className="col-12 slipheading">
-                            <h1 className='sliptitle'>Betslip</h1>
-                        </div>
-                    </div>
-                    {this.props.toBePlaced.map((item) => {
-              return (
-                    <div className="unplaced-bets row">
-                        <div className="col-12 ">
-                            {/* MAP FUNCTION */}
-                            <Cardform BetId = {item.BetId} teamName = {item.TeamName}
-                            bettingMarket = {item.BettingMarket} tournamentName ={item.TournamentName}
-                            odds = {item.Odds} stake = {item.Stake} changeReturn ={this.changeReturn}/>
-                            {/* <Cardform   />
-                            <Cardform /> */}
-                            {/* END OF MAP FUNCTION */}
-                        </div>
-                    </div>
-              )}
-              )};
-                  <div className="row">
-                        <div className="col-12 totalbetdiv">
-                            <p>TOTAL BET     £5.00</p>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-6 removealldiv">
-                            <a className="btn btn-default btn-work removeall" href="#">Remove All</a>
-                        </div>
-                        <div className="col-6">
-                            <a className="btn btn-default btn-placebet" href="#">Place Bet</a>
-                        </div>
-                    </div>
-                    <div className="row activebetdiv ">
-                        <p className='activebet'>ACTIVE BETS</p>
-                    </div>
-                </div>
-            </div>
-            </div>  
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.changeReturn = this.changeReturn.bind(this);
+    this.handlePlacedBets = this.handlePlacedBets.bind(this);
+  }
+  changeReturn(val, id) {
+    this.props.insertStake(val, id);
   }
 
-  
-  const mapStateToProps = state => ({
-    toBePlaced:state.betslip.toBePlaced,
-    activeBets:state.betslip.data.activeBets,
-    loading:state.betslip.loading,
-    error:state.betslip.error
-  });
-  
-  const mapDispatchToProps = dispatch => ({
-    insertStake: (stake,id) => {
-      dispatch(insertStake(stake,id));
-    }
-  })
-  
-  export default connect(mapStateToProps,mapDispatchToProps)(Betslip);
+  handlePlacedBets() {
+    const bets = this.props.toBePlaced;
+    this.props.placeBets(bets);
+    this.props.postBets(bets);
+  }
+
+  render() {
+    return (
+      <div className="betslip-outer col-3">
+        <div className="betslip row">
+          <div className="col-12 betmain">
+            <div className="row">
+              <div className="col-12 slipheading">
+                <h1 className="sliptitle">Betslip</h1>
+              </div>
+            </div>
+            {this.props.toBePlaced.map(item => {
+              return (
+                <div className="unplaced-bets row">
+                  <div className="col-12 ">
+                    {/* MAP FUNCTION */}
+                    <Cardform
+                      BetId={item.BetId}
+                      teamName={item.TeamName}
+                      bettingMarket={item.BettingMarket}
+                      tournamentName={item.TournamentName}
+                      odds={item.Odds}
+                      stake={item.Stake}
+                      changeReturn={this.changeReturn}
+                    />
+                    {/* <Cardform   />
+                            <Cardform /> */}
+                    {/* END OF MAP FUNCTION */}
+                  </div>
+                </div>
+              );
+            })};
+            <div className="row">
+              <div className="col-12 totalbetdiv">
+                <p>TOTAL BET £5.00</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-6 removealldiv">
+                <a className="btn btn-default btn-work removeall" href="#">
+                  Remove All
+                </a>
+              </div>
+              <div className="col-6">
+                <a
+                  className="btn btn-default btn-placebets"
+                  href="#"
+                  onClick={this.handlePlacedBets}
+                >
+                  Place Bet
+                </a>
+              </div>
+            </div>
+            <div className="row activebetdiv ">
+              <p className="activebet">ACTIVE BETS</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  toBePlaced: state.betslip.toBePlaced,
+  activeBets: state.betslip.data.activeBets,
+  loading: state.betslip.loading,
+  error: state.betslip.error
+});
+
+const mapDispatchToProps = dispatch => ({
+  insertStake: (stake, id) => {
+    dispatch(insertStake(stake, id));
+  },
+  placeBets: bets => {
+    dispatch(placeBets(bets));
+  },
+  postBets: bets => {
+    dispatch(postBets(bets));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Betslip);
--- src/reducers/betSlipReducer.js
+++ src/reducers/betSlipReducer.js
@@ -1,69 +1,77 @@
-import * as types from '../actions/types';
-import uid from 'uid'; 
+import * as types from "../actions/types";
+import uid from "uid";
 
 export const getInitialState = () => ({
-    loading: false,
-    toBePlaced: [{
+  loading: false,
+  toBePlaced: [
+    {
       BetId: 1,
-      TeamName: 'TeamName',
-      BettingMarket: 'BettingMarket',
-      TournamentName: 'TournamentName',
+      TeamName: "TeamName",
+      BettingMarket: "BettingMarket",
+      TournamentName: "TournamentName",
       Stake: 0,
-      Return: 'Return',
+      Return: "Return",
       Odds: 4,
-      fraction:4/1,
-      loss:null,
-      win:null,
-      }
-    ],
+      fraction: 4 / 1,
+      loss: null,
+      win: null
+    }
+  ],
 
-    data: {
-      activeBets:[]
-    },
-    
-    error: null
+  data: {
+    activeBets: []
+  },
 
+  error: null
 });
 
 export default (prevState = getInitialState(), action) => {
-  console.log(action,'*****')
+  console.log(action, "*****");
+
   switch (action.type) {
-          case types.INSERT_STAKE: 
-          const newState = Object.assign({}, prevState)
-          newState.toBePlaced = prevState.toBePlaced.slice()
-          const arr = newState.toBePlaced
-          for(var i=0; i< arr.length; i++){
-           if (arr[i].BetId === action.id){
-      arr[i].Stake = action.payload;
+    case types.INSERT_STAKE:
+      const newState = Object.assign({}, prevState);
+      newState.toBePlaced = prevState.toBePlaced.slice();
+      const arr = newState.toBePlaced;
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i].BetId === action.id) {
+          arr[i].Stake = action.payload;
+        }
       }
-          }
-          return newState;
+      return newState;
 
-          case types.CREATE_BET:
-          let newbet = {
-            BetId: uid(10),
-            TeamName: 'TeamName',
-            BettingMarket: 'BettingMarket',
-            TournamentName: 'TournamentName',
-            Stake: 0,
-            Return: 'Return',
-            Odds: 4,
-            fraction:4/1,
-            loss:null,
-            win:null,
-            }
-          console.log(action.payload)
-          if (Array.isArray(action.payload)) {console.log('firstblood or match duration')
-            newbet.TeamName = 'Any';
-         } 
-          else {console.log('team to win')
-         }
-          return Object.assign({}, prevState, {
-            toBePlaced: prevState.toBePlaced.concat(newbet)
-          });
+    case types.CREATE_BET:
+      let newbet = {
+        BetId: uid(10),
+        TeamName: "TeamName",
+        BettingMarket: "BettingMarket",
+        TournamentName: "TournamentName",
+        Stake: 0,
+        Return: "Return",
+        Odds: 4,
+        fraction: 4 / 1,
+        loss: null,
+        win: null
+      };
 
-      default: 
-       return prevState;
+      console.log(action.payload);
+      if (Array.isArray(action.payload)) {
+        console.log("firstblood or match duration");
+        newbet.TeamName = "Any";
+      } else {
+        console.log("team to win");
+      }
+      return Object.assign({}, prevState, {
+        toBePlaced: prevState.toBePlaced.concat(newbet)
+      });
+
+    case types.PLACE_BETS:
+      let newState = Object.assign({}, prevState);
+      newState.data.activeBets.concat(newState.toBePlaced);
+      newState.toBePlaced = [];
+      return newState;
+
+    default:
+      return prevState;
   }
-}
-
+};
